f8a40dbba8da5c8865819083f4d6aee55babb884
Andy Brown <andy.brown@candyspace.com>
2024-09-05 08:48:02 +0100

    Add basic pages return

--- charge-finder-bff/main.go
+++ charge-finder-bff/main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 
 	"charge-finder-bff/models"
 )
 
 func main() {
-	page := models.Page{
-		Name:     "home",
-		Slug:     "/home",
-		PageType: models.List,
+	router := gin.Default()
+	router.GET("/pages", getPages)
+	router.Run("localhost:8080")
+}
+
+func getPages(c *gin.Context) {
+	pages := []models.Page{
+		{
+			Name:     "Home",
+			Slug:     "home",
+			PageType: models.List,
+		},
 	}
-	fmt.Println("Page:", page)
+	c.IndentedJSON(http.StatusOK, pages)
 }
