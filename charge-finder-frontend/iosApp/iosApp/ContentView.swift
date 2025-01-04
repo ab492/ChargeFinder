563a59978ed74c614d1a525f03e8925abf7a636a
Andy Brown <35888770+ab492@users.noreply.github.com>
2025-01-04 06:52:42 +0000

    - f remove unused code from ContentView

--- charge-finder-frontend/iosApp/iosApp/ContentView.swift
+++ charge-finder-frontend/iosApp/iosApp/ContentView.swift
@@ -1,26 +1,8 @@
 import SwiftUI
-import shared
 
 struct ContentView: View {
-	let greet = Greeting().greet()
-    let api = ChargeFinderBffImpl()
-
 	var body: some View {
         ChargingStationListView()
-//		Text(greet)
-//            .onAppear {
-//                api.fetchHome { items, error in
-//                    print("ITEMS:\(items)")
-//                }
-//                
-////                api.fetchList(href: "home") { items, error in
-////                    print("ITEMS:\(items)")
-////                }
-////                
-////                api.fetchDetail(href: "chargingStationDetail/2") { detail, error in
-////                    print("DETAIL: \(detail?.description_)")
-////                }
-//            }
 	}
 }
 
@@ -28,22 +10,4 @@ struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
 		ContentView()
 	}
-}
-
-enum PageTypeSwift {
-    case list
-    
-    init?(pageType: PageType) {
-        if pageType == PageType.list {
-            self = .list
-        } else {
-            return nil
-        }
-    }
-}
-
-extension PageType {
-    var asPageTypeSwift: PageTypeSwift? {
-        PageTypeSwift(pageType: self)
-    }
 }
