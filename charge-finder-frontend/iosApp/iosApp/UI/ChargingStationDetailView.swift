452c1d83536420cd786195f27c159e3799ca99ff
Andy Brown <35888770+ab492@users.noreply.github.com>
2025-01-13 06:39:11 +0000

    - f ios add basic detail view model

--- charge-finder-frontend/iosApp/iosApp/UI/ChargingStationDetailView.swift
+++ charge-finder-frontend/iosApp/iosApp/UI/ChargingStationDetailView.swift
@@ -1,11 +1,20 @@
 import SwiftUI
 
 struct ChargingStationDetailView: View {
+    @State var viewModel = ViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(viewModel.text)
     }
 }
 
 #Preview {
     ChargingStationDetailView()
+}
+
+extension ChargingStationDetailView {
+    @Observable
+    class ViewModel {
+        let text = "Charging Station Detail"
+    }
 }
