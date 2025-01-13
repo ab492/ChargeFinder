2c40397e7b874561012f95d2c65b8639beb457b1
Andy Brown <35888770+ab492@users.noreply.github.com>
2025-01-13 06:15:57 +0000

    - f embed cf list in navigation view

--- charge-finder-frontend/iosApp/iosApp/UI/ChargingStationListView.swift
+++ charge-finder-frontend/iosApp/iosApp/UI/ChargingStationListView.swift
@@ -4,19 +4,23 @@ struct ChargingStationListView: View {
     @State var viewModel = ViewModel()
     
     var body: some View {
-        Group {
-            switch viewModel.state {
-            case .loading: ProgressView()
-            case .loaded(let items):
-                ChargingStationListSuccess(chargingStations: items)
-            case .error(let error): ErrorView(
-                errorText: error,
-                action: {
-                    Task {
-                        await viewModel.fetchChargingStations()
-                    }
-                })
+        NavigationView {
+            Group {
+                switch viewModel.state {
+                case .loading: ProgressView()
+                case .loaded(let items):
+                    ChargingStationListSuccess(chargingStations: items)
+                case .error(let error): ErrorView(
+                    errorText: error,
+                    action: {
+                        Task {
+                            await viewModel.fetchChargingStations()
+                        }
+                    })
+                }
+                
             }
+            .navigationTitle("Charging Stations")
         }
         .task { await viewModel.fetchChargingStations() }
     }
