bf5fb805a5055c0499d1417da6f887b2d03946e4
Andy Brown <35888770+ab492@users.noreply.github.com>
2025-01-20 20:36:34 +0000

    * f update reposiotry to include better data and random selection

--- charge-finder-bff/models/chargingstationrepository.go
+++ charge-finder-bff/models/chargingstationrepository.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 var allImageUrls = []string{
 	"https://images.unsplash.com/photo-1600490819528-42405785433a",
 	"https://images.unsplash.com/photo-1669349412975-a9dd0d2292ee",
@@ -11,12 +17,58 @@ var allImageUrls = []string{
 	"https://images.unsplash.com/photo-1652252421025-0392a97129a3",
 }
 
-var stations = []ChargingStation{
-	{ID: "1", LocationName: "Location 1", ImageUrls: allImageUrls},
-	{ID: "2", LocationName: "Location 2", ImageUrls: allImageUrls},
-	{ID: "3", LocationName: "Location 3", ImageUrls: allImageUrls},
-	{ID: "4", LocationName: "Location 4", ImageUrls: allImageUrls},
+// Pool of titles to randomly pick from
+var titlePool = []string{
+	"MFG EV Power",
+	"Shell Recharge",
+	"ESB Energy",
+	"Tesla Supercharger",
+	"Green Charge Station",
+	"PowerUp Charging",
+	"Volt Express",
+	"EcoFlow Energy",
+	"Spark Station",
+	"Blue Energy Hub",
+	"Rapid Charge Point",
+	"ChargeGo",
+	"SwiftCharge",
+	"ElectroPoint",
+	"BrightVolt",
+	"EnergyNext",
+	"QuickVolt",
+	"HyperCharge",
+	"Ultra EV",
+	"Fusion Power Hub",
 }
+
+func getRandomSubset(slice []string, count int) []string {
+	rand.Seed(time.Now().UnixNano()) // Seed the random generator
+	rand.Shuffle(len(slice), func(i, j int) { slice[i], slice[j] = slice[j], slice[i] })
+
+	// Limit the count to the length of the slice
+	if count > len(slice) {
+		count = len(slice)
+	}
+
+	return slice[:count]
+}
+
+func generateStations(count int) []ChargingStation {
+	var stations []ChargingStation
+
+	for i := 1; i <= count; i++ {
+		station := ChargingStation{
+			ID:           fmt.Sprintf("%d", i),
+			LocationName: titlePool[rand.Intn(len(titlePool))],        // Random title from the pool
+			ImageUrls:    getRandomSubset(allImageUrls, rand.Intn(4)+1), // 1-4 random images
+		}
+		stations = append(stations, station)
+	}
+
+	return stations
+}
+
+var stations = generateStations(20)
 
 func AllStations() []ChargingStation {
 	return stations
