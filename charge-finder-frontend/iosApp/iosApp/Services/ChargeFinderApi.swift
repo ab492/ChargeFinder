690e06ee9457dafb27131499b1104fdf81666a66
Andy Brown <35888770+ab492@users.noreply.github.com>
2025-01-24 09:42:36 +0000

    * f remove suspend function launch thread restriction as per https://stackoverflow.com/questions/75689388/calling-kotlin-suspend-functions-from-swift-objective-c-is-currently-supported-o

--- charge-finder-frontend/iosApp/iosApp/Services/ChargeFinderApi.swift
+++ charge-finder-frontend/iosApp/iosApp/Services/ChargeFinderApi.swift
@@ -10,23 +10,19 @@ final class ChargeFinderApiImpl: ChargeFinderApi {
     private let sharedApi = ChargeFinderBffImpl()
     
     func fetchChargingStationList() async throws -> [ChargingStationListItem] {
-        try await Task { @MainActor in // Calling Kotlin suspend function must occur from main thread.
-            do {
-                return try await sharedApi.fetchHome().map { ChargingStationListItem(sharedModel: $0) }
-            } catch {
-                throw error
-            }
-        }.value
+        do {
+            return try await sharedApi.fetchHome().map { ChargingStationListItem(sharedModel: $0) }
+        } catch {
+            throw error
+        }
     }
     
     func fetchChargingStationDetail(id: String) async throws -> ChargingStationDetail {
-        try await Task { @MainActor in  // Calling Kotlin suspend function must occur from main thread.
-            do {
-                let detail = try await sharedApi.fetchDetail(id: id)
-                return ChargingStationDetail(sharedModel: detail)
-            } catch {
-                throw error
-            }
-        }.value
+        do {
+            let detail = try await sharedApi.fetchDetail(id: id)
+            return ChargingStationDetail(sharedModel: detail)
+        } catch {
+            throw error
+        }
     }
 }
